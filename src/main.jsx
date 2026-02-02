de9f261c1f771d5dc1b525e2f57a88fe3ea7f49d
Lipécz Detre Platón <51795664+D3TR3@users.noreply.github.com>
2026-02-02 13:43:46 +0100

    Added nested dashboard path for the DashboardSettings.jsx and the main dashboard.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,18 @@ import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from './pages/Home'
 import App from './App'
+import Dashboard from './pages/Dashboard'
+import DashboardSettings from './pages/DashboardSettings'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />} />
       <Route path='home' element={<Home />} />
+      <Route path='dashboard'>
+        <Route index element={<Dashboard />}/>
+        <Route path='settings' element={<DashboardSettings />}/>
+      </Route>
     </Routes>
   </BrowserRouter>,
 )
