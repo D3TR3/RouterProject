c8628ca2707670e2441c69b2d1b4477acdfb3185
Lipécz Detre Platón <51795664+D3TR3@users.noreply.github.com>
2026-02-02 13:36:20 +0100

    Forgot imports for Home / App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import Home from './pages/Home'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
