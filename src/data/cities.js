e5dcffac74e392db84c8444eeda355b12baace79
Lipécz Detre Platón <51795664+D3TR3@users.noreply.github.com>
2026-02-02 14:12:33 +0100

    added Cities.js with placeholder items

--- src/data/cities.js
+++ src/data/cities.js
@@ -1,5 +1,20 @@
 const CITIES = [
     {
-        slug: ""
+        slug: "new-york",
+        name: "New York",
+        country: "USA",
+        population: 10
+    },
+    {
+        slug: "nyiregyhaza",
+        name: "Nyíregyháza",
+        country: "Hungary",
+        population: 0.15
+    },
+    {
+        slug: "lisabon",
+        name: "Lisabon",
+        country: "Portugal",
+        population: 1.6
     }
 ]
