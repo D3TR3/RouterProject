8f9202e25847082d294a9b046e99aaab9797421b
Lipécz Detre Platón <51795664+D3TR3@users.noreply.github.com>
2026-02-05 08:54:45 +0100

    Added the necessary jsx to the LoginForm.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -8,10 +8,14 @@ function LoginForm() {
     return (
         <div>
             <div>
-                <input placeholder="Email..." value={email} onChange={(e) => setEmail(e.target.value)}} type="email" />
-                <input placeholder="Password" type="password" />
+                <input placeholder="Email..." value={email} onChange={(e) => setEmail(e.target.value)} type="email" />
+                <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
             </div>
+            <div>
+            <button onClick={handleConfirm} ></button>
         </div>
+        </div>
+        
     );
 }
 
